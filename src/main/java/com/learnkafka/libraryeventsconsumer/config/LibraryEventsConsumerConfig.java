51c6a698a44a94600fdd9db93d78ebdebd27ecf8
mdastgeer <mdastgeer@xiaomi.com>
2021-11-20 11:17:57 +0530

    added retry operations

--- src/main/java/com/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
+++ src/main/java/com/learnkafka/libraryeventsconsumer/config/LibraryEventsConsumerConfig.java
@@ -11,6 +11,10 @@ import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
 import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.retry.RetryPolicy;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 
 @Configuration
 @Slf4j
@@ -28,6 +32,24 @@ public class LibraryEventsConsumerConfig {
             // this is custom logging if we want to perform some task like saving into db on error or some retry mechanism later on error record  we can do it other let kafka use its default one.
             log.info("exception in consumer config is {} and record is {}",thrownException,record);
         });
+        //this will help in implement the retry operation.but retry operation only can help in connection issue or some temprory issue not with actual exception
+        //if wew ill throw exception 100 time it will behave 100 times same way after 100 retry.
+        factory.setRetryTemplate(simpleRetryTemplate());
         return factory;
+    }
+
+    private RetryTemplate simpleRetryTemplate() {
+        FixedBackOffPolicy fixedBackOffPolicy = new FixedBackOffPolicy();
+        fixedBackOffPolicy.setBackOffPeriod(1000);
+        RetryTemplate retryTemplate = new RetryTemplate();
+        retryTemplate.setRetryPolicy(simpleRetryPolicy());
+        retryTemplate.setBackOffPolicy(fixedBackOffPolicy);
+        return retryTemplate;
+    }
+
+    private RetryPolicy simpleRetryPolicy(){
+        SimpleRetryPolicy simpleRetryPolicy = new SimpleRetryPolicy();
+        simpleRetryPolicy.setMaxAttempts(3);
+        return simpleRetryPolicy;
     }
 }
